5e6d38bbca1af359f393996e8f3627c366ce1eef
johnclary <john.clary@austintexas.gov>
2016-09-06 23:14:55 -0500

    coool broken test script

---
+++ socrata_publish_test.py
@@ -0,0 +1,32 @@
+import requests
+import json
+from secrets import SOCRATA_CREDENTIALS
+
+
+url = 'https://data.austintexas.gov/resource/utgi-umz5.json'
+
+auth = (SOCRATA_CREDENTIALS['user'], SOCRATA_CREDENTIALS['password'])
+
+json_data = [{
+    "atd_intersection_id":"48",
+    ":deleted": "true"
+}]
+
+res = requests.post(url, data=json_data, auth=auth)
+
+print(res.json)
+res.raise_for_status()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
