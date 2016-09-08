334b99be6104fcc3650eb83962f1a07d299df9fc
johnclary <john.clary@austintexas.gov>
2016-09-08 16:26:27 -0500

    so real yet so fake

--- signal-status/fake-secrets.py
+++ signal-status/fake-secrets.py
@@ -1,2 +1,13 @@
-GITHUB_USERNAME = 'user-name'
-GITHUB_TOKEN = 'auth-key'
+GITHUB_USERNAME = ''
+GITHUB_TOKEN = ''
+KITS_CREDENTIALS = {
+    'server': '',
+    'user': '',
+    'password': '',
+    'database': ''
+}
+
+SOCRATA_CREDENTIALS = {
+    'user': '',
+    'password': ''
+}
