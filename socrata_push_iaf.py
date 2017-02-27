47d1cdf8fe6f0eb266f9db648bc012fcdb1585ce
Itamar Gal <igal@utexas.edu>
2017-02-27 13:14:33 -0600

    Added scripts to push IAF and ITMF data to Open Data Portal.

---
+++ socrata_push_iaf.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from socrata_helpers import UpsertData
+import csv
+from secrets import SOCRATA_CREDENTIALS
+
+def upsert(input_dir, month_day_year, awam_host_instance_name="Austin"):
+
+    # Set IAF path parameters
+    iaf_filename = "%s_bt_%s.txt" % (awam_host_instance_name, month_day_year)
+    iaf_input_path = os.path.join(input_dir, iaf_filename)
+
+    # Upload the data
+    with open(iaf_input_path, 'r') as iaf_input_file:
+        reader = csv.reader(iaf_input_file)
+        fieldnames = ['record_id','host_read_time','field_device_read_time','reader_identifier','device_address']
+        data = [dict(zip(fieldnames, record)) for record in reader]
+        UpsertData(SOCRATA_CREDENTIALS, data, "qnpj-zrb9")
+
+if __name__ == '__main__':
+    input_dir = sys.argv[1]
+    month_day_year = sys.argv[2]
+    upsert(input_dir, month_day_year)
---
+++ socrata_push_itmf.py
@@ -0,0 +1,33 @@
+#!/usr/bin python
+
+import os
+import sys
+from socrata_helpers import UpsertData
+import csv
+from secrets import SOCRATA_CREDENTIALS
+
+def upsert(input_dir, month_day_year, awam_host_instance_name="Austin"):
+
+    # Set ITMF path parameters
+    itmf_filename = "%s_btmatch_%s.txt" % (awam_host_instance_name, month_day_year)
+    itmf_input_path = os.path.join(input_dir, itmf_filename)
+
+    # Upload the data
+    with open(itmf_input_path, 'r') as itmf_input_file:
+        reader = csv.reader(itmf_input_file)
+        fieldnames = [
+            'record_id','device_address','origin_reader_identifier','destination_reader_identifier','start_time',
+            'end_time','travel_time_seconds','speed_miles_per_hour','match_validity','filter_identifier'
+        ]
+        # Skip header row
+        # print(reader.next())
+        reader.next()
+        data = [dict(zip(fieldnames, record)) for record in reader]
+        # for row in data:
+        #     print(row)
+        UpsertData(SOCRATA_CREDENTIALS, data, "x44q-icha")
+
+if __name__ == '__main__':
+    input_dir = sys.argv[1]
+    month_day_year = sys.argv[2]
+    upsert(input_dir, month_day_year)
