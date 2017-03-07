1e01d691df5ce44f24557a6ceb13de3ca405e413
John Clary <john.clary@austintexas.gov>
2017-03-07 16:34:36 -0600

    create bt_summary_pub

--- bt_summary_pub.py
+++ bt_summary_pub.py
@@ -1,21 +1,27 @@
-# still need to zip up the rows
-
-
-# origin_reader_identifier, destination_reader_identifier, origin_roadway, origin_cross_street, origin_direction, destination_roadway, destination_cross_street, destination_direction, segment_length_in_miles, timestamp, average_travel_time_seconds, average_speed_mph, summary_interval_minutes, number_samples, standard_deviation
-# congress_slaughter,congress_wm_cannon,Congress,Slaughter,Northbound,Congress,William Cannon,Southbound,1.91,12/30/2015 12:00 AM,-1,-1,15,0,-1
-
 import os
 import csv
 from datetime import datetime
 import pytz
 import socrata_helpers
+import secrets
 import pdb
 
 resouce_id = 'v7zg-5jg9'
 
-fieldnames = ['origin_reader_identifier', 'destination_reader_identifier', 'origin_roadway', 'origin_cross_street', 'origin_direction', 'destination_roadway', 'destination_cross_street', 'destination_direction', 'segment_length_miles', 'timestamp', 'average_travel_time_seconds', 'average_speed_mph', 'summary_interval_minutes', 'number_samples', 'standard_deviation']
+fieldnames = ['record_id', 'origin_reader_identifier', 'destination_reader_identifier', 'origin_roadway', 'origin_cross_street', 'origin_direction', 'destination_roadway', 'destination_cross_street', 'destination_direction', 'segment_length_miles', 'timestamp', 'average_travel_time_seconds', 'average_speed_mph', 'summary_interval_minutes', 'number_samples', 'standard_deviation']
 
-rootDir = './data'
+rootDir = ''
+
+excluded = 0
+included = 0
+
+def hasSeconds(local_time_string):
+    try:
+        datetime.strptime(local_time_string, '%m/%d/%Y %I:%M:%S %p')
+        return True
+
+    except ValueError:
+        return False
 
 def get_timestamp(local_time_string):
     """
@@ -25,7 +31,12 @@ def get_timestamp(local_time_string):
     local_tz = pytz.timezone("US/Central")
 
     # Parse time string into naive/timezone-unaware datetime object
-    naive_dt = datetime.strptime(local_time_string, '%m/%d/%Y %I:%M:%S %p')
+    if hasSeconds(local_time_string):
+        naive_dt = datetime.strptime(local_time_string, '%m/%d/%Y %I:%M:%S %p')
+
+    else:
+        naive_dt = datetime.strptime(local_time_string, '%m/%d/%Y %I:%M %p')
+
 
     # Convert naive datetime object to timezone aware datetime object
     local_dt = local_tz.localize(naive_dt)
@@ -34,7 +45,7 @@ def get_timestamp(local_time_string):
     utc_dt = local_dt.astimezone(pytz.utc)
 
     # Get Unix time / posix time / epoch time
-    epoch = utc_dt.strftime('%s')
+    epoch = int( utc_dt.timestamp() )
 
     # Return epoch time
     return(str(epoch))
@@ -44,18 +55,28 @@ def get_timestamp(local_time_string):
 def process_data(filename):
 
     with open(filename, 'r') as input_file:
+        global included
+        global excluded
+        
         reader = csv.reader(input_file)
 
         rows = []
 
-        for row in data:
-            row[9] = get_timestamp(row[9])
+        for row in reader:
+            if int(row[13]) > 0:
+                included += 1
+                row[9] = get_timestamp(row[9])
 
-            # create unique row id
-            row.insert(0, '{}_{}_{}'.format(row[9], row[0], row[1]) )
+                # create unique row id
+                row.insert(0, '{}{}{}'.format(row[9], row[0], row[1]) )
 
-            rows.append(row)
+                rows.append(row)
+            else:
+                excluded += 1
 
+        print('incuded: {}'.format(included) )
+        print('excluded: {}'.format(excluded) )
+        
         return rows
 
 
@@ -63,8 +84,13 @@ def process_data(filename):
 for dirpath, subdirs, files in os.walk(rootDir):
     for fname in files:
         if 'Austin_bt_summary_' in fname:
-            data = process_data( os.path.join(in_dir, infile) )
+            print(fname)
+            data = process_data( os.path.join(dirpath, fname) )
             
             payload = [dict(zip(fieldnames, record)) for record in data]
-            print(payload)
-            pdb.set_trace()
+
+            socrata_helpers.UpsertData(secrets.SOCRATA_CREDENTIALS, payload, resouce_id)
+
+
+
+
