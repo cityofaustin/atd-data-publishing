aa4c2957d94482b6540f2b1594aa88658e3389bc
Clary <John.Clary@austintexas.gov>
2017-05-23 15:12:05 -0500

    hello worlds

---
+++ kits_sync.py
@@ -0,0 +1,49 @@
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+import arrow
+import sys
+import kits_helpers
+import knack_helpers
+import data_helpers
+import secrets
+import pdb
+
+
+query = "SELECT * FROM KITSDB.KITS.CAMERA"
+
+
+def main(date_time):
+    print('starting stuff now')
+
+    kits_data = kits_helpers.data_as_dict(kits_creds, query)
+
+
+
+
+if __name__ == '__main__':
+    
+    now = arrow.now()
+    now_s = now.format('YYYY_MM_DD')
+    
+    #  init logging 
+    #  with one logfile per dataset per day
+    cur_dir = os.path.dirname(__file__)
+    logfile = 'log/{}_{}.log'.format('kits_camera_sync', now_s)
+    log_path = os.path.join(cur_dir, logfile)
+    logging.basicConfig(filename=log_path, level=logging.INFO)
+    logging.info('START AT {}'.format(str(now)))
+
+    kits_creds = secrets.KITS_CREDENTIALS
+
+    maint(now)
+    
+#  get KITS data
+#  get KNACK data
+#  detect changes
+#  new in KITS
+#  missing from KITS
+#  delete from KITS (eek!)
+#  log results
+#  email when camera is created?
