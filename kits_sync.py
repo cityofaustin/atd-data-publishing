edf07a580bed9a1bc14365e879db3d57c69114f8
johnclary <john.clary@austintexas.gov>
2017-05-23 19:01:09 -0500

    progreso

--- kits_sync.py
+++ kits_sync.py
@@ -1,3 +1,7 @@
+#  TODO
+#  location naming >> need prefix?
+#  update lat/lon? (yes)
+
 if __name__ == '__main__' and __package__ is None:
     from os import sys, path
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
