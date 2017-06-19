6a10fc5b1658d2954bd99f53578e5a9f32234e99
Clary <John.Clary@austintexas.gov>
2017-06-19 16:34:28 -0500

    remove this file

--- cifs.py
+++ cifs.py
@@ -1,15 +1,17 @@
 import pdb
 import logging
 import json
+import csv
 import arrow
 import agol_helpers
-from secrets import AGOL_CREDENTIALS as creds
+import socrata_helpers
+from secrets import SOCRATA_CREDENTIALS as socrata_creds
+from secrets import AGOL_CREDENTIALS as agol_creds
+from secrets import LOG_DIRECTORY as log_directory
 '''
 TODO:
-- upload to socrata
+- geocoding
 '''
-log_directory = '.'
-
 now = arrow.now()
 now_s = now.format('YYYY_MM_DD')
 now_mills = now.timestamp * 1000
@@ -18,14 +20,18 @@ logfile = '{}/cifs_{}.log'.format(log_directory, now_s)
 logging.basicConfig(filename=logfile, level=logging.INFO)
 logging.info('START AT {}'.format(now.format()))
 
-service_url = 'http://services.arcgis.com/0L95CJ0VTaxqcmED/arcgis/rest/services/ATD_road_closures_incidents/FeatureServer/0/'
+socrata_resource_id_json = 'fwsr-gb9r'
+socrata_resource_id_csv = 'aki2-nu5c'
 
-query_params = {
-    'f' : 'json',
-    'where' : 'EVENT_START_DATETIME IS NOT NULL AND EVENT_END_DATETIME IS NOT NULL',
-    'time' : now_mills,
-    'outFields' : '*',
-    'returnGeometry' : True
+agol_config = {
+    'service_url' : 'http://services.arcgis.com/0L95CJ0VTaxqcmED/arcgis/rest/services/ATD_road_closures_incidents/FeatureServer/0/',
+    'query_params' : {
+        'f' : 'json',
+        'where' : 'EVENT_START_DATETIME IS NOT NULL AND EVENT_END_DATETIME IS NOT NULL',
+        'time' : now_mills,
+        'outFields' : '*',
+        'returnGeometry' : True
+    }   
 }
 
 reference_id = '1501'  # arbitrary org reference
@@ -68,12 +74,12 @@ fieldmap = {
         'cifs_name' : 'endtime',
         'required' : True
     },
-    'CreationDate' : {
+    'CREATED_DATE' : {
         'type' : 'datetime',
         'cifs_name' : 'creationtime',
         'required' : True
     },
-    'EditDate' : {
+    'MODIFIED_DATE' : {
         'type' : 'datetime',
         'cifs_name' : 'updatetime',
         'required' : True
@@ -82,8 +88,9 @@ fieldmap = {
 
 incidents = [] 
 
+
 def convertToIso(mills):
-    return arrow.get(mills/1000).format("YYYY-MM-DDTHH:mm:ssZZ") #  iso-8601
+    return arrow.get(mills/1000).to('US/Central').format("YYYY-MM-DDTHH:mm:ssZZ") #  iso-8601
 
 
 def mapfields(feature, fieldmap):
@@ -97,7 +104,6 @@ def mapfields(feature, fieldmap):
     return new_feature
     
 
-
 def buildPolyline(feature):
     paths = feature['geometry']['paths']
     
@@ -109,16 +115,40 @@ def buildPolyline(feature):
     for path in paths:
         for point in path:
             for coord in point:
-                print(coord)
                 polyline.append(coord)
 
     return ' '.join(str(coord) for coord in polyline)
 
 
+def convertToCsv(incidents):
+    rows = []
+    fieldnames = []
+
+    for feature in incidents:
+        row = {}
+        for field in feature.keys():
+            if field == '-id':
+                feature['id'] = feature.pop(field)
+                field = 'id'
+
+            if type(feature[field]) == dict:
+                for subfield in feature[field].keys():
+                    if subfield not in fieldnames:
+                        fieldnames.append(subfield)
+                    row[subfield] = feature[field][subfield]
+            
+            else:
+                if field not in fieldnames:
+                    fieldnames.append(field)
+                row[field] = feature[field]
+        rows.append(row)
+    return (rows, fieldnames)
+
+
 def main():
     #  get closure data
-    query_params['token'] = agol_helpers.get_token(creds)
-    data = agol_helpers.query_layer(service_url, query_params)
+    agol_config['query_params']['token'] = agol_helpers.get_token(agol_creds)
+    data = agol_helpers.query_layer(agol_config['service_url'], agol_config['query_params'])
     
     logging.info(str(len( data['features'] )))
     
@@ -130,16 +160,41 @@ def main():
             'street' : incident.pop('street'),
             'polyline' : incident.pop('polyline')
         }
+
         incident['location'] = location
-        #  now add 'missing' fields and nest fields as needed
+        
+        incident['source'] = {
+            'reference': reference_id,
+            'name' : reference_name
+        }
+
         incidents.append(incident)
     
-    #  write to json
-    with open('traffic-incidents.json', 'w') as outfile:
-        json.dump(incidents, outfile)
+    
+    #  write to socrata json feed
+    file_string = json.dumps(incidents)
+    upload_response = socrata_helpers.replace_non_data_file(socrata_creds, socrata_resource_id_json, 'traffic-incidents.json', file_string)
+
+    # #  write to json
+    # with open('traffic-incidents.json', 'w') as outfile:
+    #     json.dump(incidents, outfile)
+
+    #  write to csv
+    csv_data = convertToCsv(incidents)
+    
+    # with open('traffic-incidents.csv', 'w') as outfile:
+    #     fieldnames = csv_data[1]
+    #     writer = csv.DictWriter(outfile, fieldnames=fieldnames, lineterminator="\n")
+    #     writer.writeheader()
+    #     writer.writerows([ row for row in csv_data[0] ])
+
+    #  write to socrata csv
+    replace_response = socrata_helpers.replace_data(socrata_creds, socrata_resource_id_csv, csv_data[0])
+
 
 main()
 
+logging.info(arrow.now().format())
 
 
 # def build_incident(feature, fieldmap):
