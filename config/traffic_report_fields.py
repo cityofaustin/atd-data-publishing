0114b51c19b5c8558e8204a0bcc721f5e9e0510a
John Clary <john.clary@austintexas.gov>
2017-10-24 22:16:03 -0500

    Add Status Date to Traffic Incident Reports We're scraping a traffic incident feed from city's computer automated dispatch system. When incidents are removed from the feed, we assign them a status of archived. With this commit we update a TRAFFIC_REPORT_STATUS_DATE_TIME field with the date/time at which we archive the record. This will allow us to query for incidents that were recently closed and dispaly them on our operations map, etc.

--- config/traffic_report_fields.py
+++ config/traffic_report_fields.py
@@ -1,12 +1,7 @@
 #  Field metadata for the traffic_report object in the AMD data tracker.
 #  Storing the metadata here averts the need to make frquent private API
 #  calls to the Knack API.
-TRAFFIC_REPORT_META = {'field_1826': {'label': 'TRAFFIC_REPORT_ID', 'key': 'field_1826', 'required': False, 'type': 'short_text'}, 'field_1833': {'label': 'FOUND_ADDRESS', 'key': 'field_1833', 'required': False, 'type': 'short_text'}, 'field_1827': {'label': 'PUBLISHED_DATE', 'key': 'field_1827', 'required': False, 'type': 'date_time'}, 'field_1828': {'label': 'ADDRESS', 'key': 'field_1828', 'required': False, 'type': 'short_text'}, 'field_1829': {'label': 'ISSUE_REPORTED', 'key': 'field_1829', 'required': False, 'type': 'short_text'}, 'field_1831': {'label': 'LOCATION', 'key': 'field_1831', 'required': False, 'type': 'address'}, 'field_1832': {'label': 'GEOCODE_SCORE', 'key': 'field_1832', 'required': False, 'type': 'number'}, 'field_1837': {'label': 'LATITUDE', 'key': 'field_1837', 'required': False, 'type': 'number'}, 'field_1838': {'label': 'LONGITUDE', 'key': 'field_1838', 'required': False, 'type': 'number'}, 'field_1843': {'label': 'TRAFFIC_REPORT_STATUS', 'key': 'field_1843', 'required': False, 'type': 'multiple_choice', 'choices': ['NEW', 'ARCHIVED']}, 'id': {'key': 'id', 'label': 'id', 'type': 'id'}}
+TRAFFIC_REPORT_META = {'field_1826': {'label': 'TRAFFIC_REPORT_ID', 'key': 'field_1826', 'required': False, 'type': 'short_text'}, 'field_1827': {'label': 'PUBLISHED_DATE', 'key': 'field_1827', 'required': False, 'type': 'date_time'}, 'field_1828': {'label': 'ADDRESS', 'key': 'field_1828', 'required': False, 'type': 'short_text'}, 'field_1829': {'label': 'ISSUE_REPORTED', 'key': 'field_1829', 'required': False, 'type': 'short_text'}, 'field_1831': {'label': 'LOCATION', 'key': 'field_1831', 'required': False, 'type': 'address'}, 'field_1837': {'label': 'LATITUDE', 'key': 'field_1837', 'required': False, 'type': 'number'}, 'field_1838': {'label': 'LONGITUDE', 'key': 'field_1838', 'required': False, 'type': 'number'}, 'field_1843': {'label': 'TRAFFIC_REPORT_STATUS', 'key': 'field_1843', 'required': False, 'type': 'multiple_choice', 'choices': ['ACTIVE', 'ARCHIVED']}, 'field_1966': {'label': 'TRAFFIC_REPORT_STATUS_DATE_TIME', 'key': 'field_1966', 'required': False, 'type': 'date_time'}, 'id': {'key': 'id', 'label': 'id', 'type': 'id'}}
 
 
-
-
-
-
-
-
+                    
