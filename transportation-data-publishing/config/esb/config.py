d2542407e021b73678ed69f94573db5852f11a6f
John Clary <john.clary@austintexas.gov>
2019-07-21 15:59:10 -0500

    reworking config to abstract more details

--- transportation-data-publishing/config/esb/config.py
+++ transportation-data-publishing/config/esb/config.py
@@ -10,5 +10,23 @@ cfg = {
         "esb_status_match": "READY_TO_SEND",
         "path_cert": "../config/esb/cert.pem",
         "path_key": "../config/esb/key.pem",
+        "activity_details_fieldname" : "TMC_ACTIVITY_DETAILS".
+        "activity_name_fieldname" : "TMC_ACTIVITY",
+        "activity_id_field" : "ATD_ACTIVITY_ID"
+    },
+    "signs_markings_activities": {
+        "obj": "",
+        "ref_obj": ["", ""],
+        "scene": "",
+        "view": "",
+        "template": "../config/esb/esb_csr_template_signsmarkings.xml",
+        "emi_field": "",
+        "esb_status_field": "",
+        "esb_status_match": "READY_TO_SEND",
+        "path_cert": "../config/esb/cert.pem",
+        "path_key": "../config/esb/key.pem",
+        "activity_details_fieldname" : "ACTIVITY_DETAILS".
+        "activity_name_fieldname" : "ACTIVITY",
+        "activity_id_field" : "ATD_ACTIVITY_ID"
     }
 }
