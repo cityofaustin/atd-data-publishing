0c39f98391e77f60ae101913847821bec11a770a
johnclary <john.clary@austintexas.gov>
2019-12-09 21:08:09 -0600

    bare bones

---
+++ transportation-data-publishing/data_tracker/markigns_work_order_flex_notes.py
@@ -0,0 +1,54 @@
+"""
+Connect markings work orders  to the flex notes that are connected to their connected
+311 Service Request.
+"""
+
+import pdb
+
+import argutil
+import knackpy
+import knackutil
+
+import _setpath
+from config.knack.config import cfg
+from config.secrets import *
+
+
+def knackpy_wrapper(cfg_dataset, auth):
+    return knackpy.Knack(
+        scene=cfg_dataset["scene"],
+        view=cfg_dataset["view"],
+        ref_obj=cfg_dataset["ref_obj"],
+        app_id=auth["app_id"],
+        api_key=auth["api_key"],
+    )
+
+
+def main():
+
+    args = cli_args()
+    
+    auth = KNACK_CREDENTIALS[args.app_name]
+
+    kn = knackpy_wrapper(cfg_dataset, auth)
+
+    if not kn.data:
+        return 0
+
+    return len(kn.data)
+
+
+def cli_args():
+    parser = argutil.get_parser(
+        "markings_awork_order_flex_notes.py",
+        "Connect markings work orders to their related flex notes.",
+        "app_name"
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    main()
