99e59e8c1cd026736c601dc56d32d866d72b068d
johnclary <john.clary@austintexas.gov>
2019-12-10 15:42:45 -0600

    linting w/ black

--- transportation-data-publishing/config/knack/config.py
+++ transportation-data-publishing/config/knack/config.py
@@ -783,20 +783,20 @@ SR_ASSET_ASSIGNER = {
 }
 
 MARKINGS_WORK_ORDERS_FLEX_NOTES = {
-    "flex_notes" : {
+    "flex_notes": {
         "scene": "scene_1271",
         "view": "view_3169",
         "obj": None,
         "ref_obj": ["object_172"],
         "sr_number_field_id": "field_3128",
-        "work_order_connection_field_id" : "field_3629",
-        "payload_field_ids" : ["id", "field_3629"]
+        "work_order_connection_field_id": "field_3629",
+        "payload_field_ids": ["id", "field_3629"],
     },
-    "work_orders" : {
-        "sr_number_field_name" : "SR_NUMBER_csr",
+    "work_orders": {
+        "sr_number_field_name": "SR_NUMBER_csr",
         "scene": "scene_1271",
         "view": "view_3170",
-        "obj" : None,
+        "obj": None,
         "ref_obj": ["object_140", "object_171"],
-    }
+    },
 }
