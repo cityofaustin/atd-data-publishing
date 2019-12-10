0d2449fc23f81d06b1a2cc54a553435eb9fa6339
johnclary <john.clary@austintexas.gov>
2019-12-10 15:42:29 -0600

    ready for testing

--- transportation-data-publishing/data_tracker/markings_work_order_flex_notes.py
+++ transportation-data-publishing/data_tracker/markings_work_order_flex_notes.py
@@ -1,9 +1,7 @@
 """
-Connect markings work orders  to the flex notes that are connected to their connected
+Connect markings work orders to the flex notes that are connected to their connected
 311 Service Request.
 """
-
-# todo: created a test CSR: 19-00280974. But we're waiting for the ISSUE_TYPE to popualte through to the flext notes, i think.
 
 from pprint import pprint as print
 import pdb
@@ -17,15 +15,20 @@ from config.knack.config import MARKINGS_WORK_ORDERS_FLEX_NOTES as cfg
 from config.secrets import *
 
 
-def knackpy_wrapper(cfg_dataset, auth):
+def filter_flex_note(field, value):
+    return [{"field": f"{field}", "operator": "contains", "value": f"{value}"}]
+
+
+def knackpy_wrapper(cfg, auth, filters=None):
+
     return knackpy.Knack(
-        scene=cfg_dataset["scene"],
-        view=cfg_dataset["view"],
-        ref_obj=cfg_dataset["ref_obj"],
+        scene=cfg["scene"],
+        view=cfg["view"],
+        obj=cfg["obj"],
+        ref_obj=cfg["ref_obj"],
         app_id=auth["app_id"],
         api_key=auth["api_key"],
-        rows_per_page=10,  # TODO: remove record limits
-        page_limit=1,  # TODO: remove record limits
+        filters=filters,
     )
 
 
@@ -35,21 +38,50 @@ def main():
 
     auth = KNACK_CREDENTIALS[args.app_name]
 
-    kn_work_orders = knackpy_wrapper(cfg["work_orders"], auth)
+    # gets work orders have a connected CSR, but no flex notes
+    # this filtering is done in the knack view
+    work_orders = knackpy_wrapper(cfg["work_orders"], auth)
 
-    pdb.set_trace()
-
-    
-    """
-    TODO:
-    - extract knack work order id
-    - get each flex note record id for SR number of connected work order
-    - update flex note work order connection field with work order knack record id
-    """
-    if not kn.data:
+    if not work_orders.data:
         return 0
 
-    return len(kn.data)
+    for wo in work_orders.data:
+        payload = []
+
+        sr_number = wo.get(cfg["work_orders"]["sr_number_field_name"])
+
+        wo_id = wo.get("id")
+
+        # fetch all flex notes connected to each service request
+        # the Knack view is filtered to exclude the "XFERIOSK" flex note
+        # which is just "allow record to be published to external interfaces"
+        _filter = filter_flex_note(cfg["flex_notes"]["sr_number_field_id"], sr_number)
+
+        flex_notes = knackpy_wrapper(cfg["flex_notes"], auth, filters=_filter)
+
+        for fn in flex_notes.data_raw:
+            fn[cfg["flex_notes"]["work_order_connection_field_id"]] = [wo_id]
+
+            # reduce flex note to record id, work order connection field
+            fn_payload = {}
+
+            for key in fn.keys():
+                if key in cfg["flex_notes"]["payload_field_ids"]:
+                    fn_payload[key] = fn[key]
+
+            payload.append(fn_payload)
+
+        for record in payload:
+
+            res = knackpy.record(
+                record,
+                obj_key=cfg["flex_notes"]["ref_obj"][0],
+                app_id=auth["app_id"],
+                api_key=auth["api_key"],
+                method="update",
+            )
+
+    return len(work_orders.data)
 
 
 def cli_args():
