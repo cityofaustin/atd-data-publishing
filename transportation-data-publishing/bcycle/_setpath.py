437a491aca254c394f2df08de953979c0749b6ea
John Clary <john.clary@austintexas.gov>
2021-12-22 12:15:02 -0600

    remove deprecated scripts

--- transportation-data-publishing/bcycle/_setpath.py
+++
@@ -1,8 +0,0 @@
-# update current path to include util directory
-# appends parent directory to path
-# used to just append '..' to path, but that breaks when
-# script is imoported by an external package
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
