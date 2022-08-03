7a31b4f218fd68ffc1c33ccbe045c1c37dba7afa
John Clary <john.clary@austintexas.gov>
2022-08-03 17:18:37 -0400

    Use proxied-postgrest endpoint

--- transportation-data-publishing/config/postgrest/config.py
+++ transportation-data-publishing/config/postgrest/config.py
@@ -1,6 +1,6 @@
 PGREST_PUB = {
     "traffic_reports": {
-        "pgrest_base_url": "https://atd-postgrest.austinmobility.io/traffic_reports",
+        "pgrest_base_url": "https://atd-postgrest.austinmobility.io/legacy-scripts/traffic_reports",
         "primary_key": "traffic_report_id",
         'limit' : 2000000,
         "modified_date_field": "traffic_report_status_date_time",
@@ -20,7 +20,7 @@ PGREST_PUB = {
 
 TRAFFIC_REPORT_SCRAPER = {
     "feed_url": "http://www.ci.austin.tx.us/qact/qact_rss.cfm",
-    "endpoint": "https://atd-postgrest.austinmobility.io/traffic_reports",
+    "endpoint": "https://atd-postgrest.austinmobility.io/legacy-scripts/traffic_reports",
     "primary_key": "traffic_report_id",
     "status_field": "traffic_report_status",
     "date_field": "published_date",
